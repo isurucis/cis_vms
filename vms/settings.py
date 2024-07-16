c89b6316244ca61f6f49477873dac34f82959fb2
isurucis <143800833+isurucis@users.noreply.github.com>
2024-07-16 23:47:43 +0530

    Update settings.py
    update db setting

--- vms/settings.py
+++ vms/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'vms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cisvms$default',
+        'USER': 'cisvms',
+        'PASSWORD': 'iS123456',
+        'HOST': 'cisvms.mysql.pythonanywhere-services.com',
     }
 }
 
